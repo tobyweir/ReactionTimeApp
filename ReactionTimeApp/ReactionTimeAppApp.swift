5ac9015adbde8bef393d2b9b220765e23ab44ec2
Toby Weir <tobyweir3@gmail.com>
2025-06-23 14:16:53 +0100

    basic setup for project

---
+++ ReactionTimeApp/ReactionTimeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReactionTimeAppApp.swift
+//  ReactionTimeApp
+//
+//  Created by Toby Weir on 23/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct ReactionTimeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
