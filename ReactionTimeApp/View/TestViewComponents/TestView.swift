50c1411dfe1e58362ec825e4c4831ff2ae0c91dc
Toby Weir <tobyweir3@gmail.com>
2025-07-07 12:33:10 +0100

    initial commit for the TestView

---
+++ ReactionTimeApp/View/TestViewComponents/TestView.swift
@@ -0,0 +1,18 @@
+//
+//  TestView.swift
+//  ReactionTimeApp
+//
+//  Created by Toby Weir on 07/07/2025.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestView()
+}
