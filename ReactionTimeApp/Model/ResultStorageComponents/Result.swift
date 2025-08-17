d966e0815dacb841142026733dca7d4ab59c55a8
Toby Weir <tobyweir3@gmail.com>
2025-08-17 15:57:20 +0100

    DayOverView is almost there, needs tweaking with colours and the table but feature complete.

--- ReactionTimeApp/Model/ResultStorageComponents/Result.swift
+++ ReactionTimeApp/Model/ResultStorageComponents/Result.swift
@@ -10,7 +10,7 @@ import Foundation
 //Simple Definition of what a result is,
 //To be used by Controller and ResultStore for storage
 //And to be displayed by Calendar and Graph views 
-struct Result: Codable {
+struct Result: Codable, Identifiable {
     var id = UUID()
     var time: Double
     var dateRecorded: Date = Date.now
--- ReactionTimeApp/View/CalendarViewComponents/DayOverView/DayOverView.swift
+++ ReactionTimeApp/View/CalendarViewComponents/DayOverView/DayOverView.swift
@@ -9,17 +9,22 @@ import SwiftUI
 struct DayOverView: View {
     let date: Date
     let model: Controller
-    let cornerRadius: CGFloat = 50
-
+    let cornerRadius: CGFloat = 40
+    let listColour1: Color = .gray
+    let listColour2: Color = .white
     var foregroundColour: Color {
-        .red
+        .gray.opacity(1)
     }
 
     var backgroundColour: Color {
-        .blue
+        .red.opacity(1)
     }
     var results: [Result] {
         model.getResults(on: date)
+    }
+
+    var bestResult: Double? {
+        results.sorted(by: {$0.time < $1.time}).first?.time
     }
 
     var resultAverage: Double {
@@ -43,6 +48,7 @@ struct DayOverView: View {
                 Spacer()
             } else {
                 infoBlocks
+                    .padding(.vertical)
             }
         }
         .padding(.horizontal)
@@ -58,6 +64,7 @@ struct DayOverView: View {
             Text("\(date.formatted(Date.FormatStyle().weekday(.wide).day()).uppercased())")
                 .font(.system(size: 15))
                 .padding(.horizontal)
+                .foregroundStyle(foregroundColour)
             Spacer()
         }
         HStack {
@@ -95,20 +102,75 @@ struct DayOverView: View {
         ZStack {
             RoundedRectangle(cornerRadius: cornerRadius)
                 .foregroundStyle(backgroundColour)
+            VStack  {
+                HStack {
+                    Text("Result count:")
+                        .padding(.horizontal)
+                        .foregroundStyle(foregroundColour)
+                    Spacer()
+                }
+                Spacer()
+                Text("\(results.count)")
+                    .font(.system(size: 30, weight: .bold, design: .rounded))
+                    .foregroundStyle(.black)
+                Spacer()
+            }.padding(.vertical)
         }
     }
 
     var dayAverageBlock: some View {
         ZStack {
             RoundedRectangle(cornerRadius: cornerRadius)
+                .foregroundStyle(backgroundColour)
+            VStack {
+                HStack {
+                    Text("Average:")
+                        .foregroundStyle(foregroundColour)
+                        .padding(.horizontal)
+                    Spacer()
+                }
+                Spacer()
+                Text("\(Int(resultAverage * 1000))ms")
+                    .foregroundStyle(.black)
+                    .font(.system(size: 30, weight: .bold, design: .rounded))
+
+                Spacer()
+            }.padding(.vertical)
         }
     }
 
     var scrollableResultsBlock: some View {
         ZStack {
             RoundedRectangle(cornerRadius: cornerRadius)
-            ScrollView  {
+                .foregroundStyle(backgroundColour)
+            resultList
+        }
+        .padding(10)
+    }
 
+    var resultList: some View {
+        GeometryReader { proxy in
+            Table(results) {
+                TableColumn("Result") { result in
+                    Text("\(Int (result.time * 1000))ms")
+                }
+                TableColumn("Time Recorded") { result in
+                    Text("\(result.dateRecorded.formatted(Date.FormatStyle().hour().minute()))")
+                }
+            }
+//            .frame(width: proxy.size.width, height: proxy.size.height)
+            .scaleEffect(0.85 , anchor: .center)
+        }
+    }
+
+    struct resultItemView: View {
+        let result: Result
+        var body: some View {
+            HStack {
+                Text("\(Int(result.time * 1000))ms")
+                    .foregroundStyle(.black)
+                Spacer()
+                Text("\(result.dateRecorded.formatted(Date.FormatStyle().hour().minute()))")
             }
         }
     }
@@ -119,6 +181,7 @@ struct DayOverView: View {
                 .foregroundStyle(backgroundColour)
             VStack {
                 Text("Grade:")
+                    .foregroundStyle(foregroundColour)
                 Spacer()
                 Text("\(Result.getAnimalGrade(for: resultAverage))")
                 Spacer()
@@ -130,6 +193,20 @@ struct DayOverView: View {
     var bestResultOfTheDayBlock: some View {
         ZStack {
             RoundedRectangle(cornerRadius: cornerRadius)
+                .foregroundStyle(backgroundColour)
+            VStack {
+                HStack {
+                    Text("Best Result:")
+                        .foregroundStyle(foregroundColour)
+                        .padding(.horizontal)
+                    Spacer()
+                }
+                Spacer()
+                Text("\(Int( (bestResult ?? 0.0) * 1000) )ms")
+                    .foregroundStyle(.black)
+                    .font(.system(size: 30, weight: .bold, design: .rounded))
+                Spacer()
+            }.padding(.vertical)
         }
     }
 
